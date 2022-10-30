ed2bf911ceaa7f5257b1c6de6538d3a2b71382ce
kawamuragen <genn.kawamura@gmail.com>
2022-10-30 17:07:07 +0900

    Up to lesson 11.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import AutoBatchEventHandler from "./components/AutoBatchEventHandler";
 
 function App() {
+  // useEffectの２引数を[]にするとコンポーネントを読み込んだ最初だけ実行される。
+  useEffect(() => {
+    console.log("Appがレンダリングされた。React17では1度だけ呼ばれる。");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AutoBatchEventHandler />
     </div>
   );
 }
